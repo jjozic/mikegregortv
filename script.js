91b07973fe48cdd7036afa0f926fbd7c9de0038b
Julian Jozic <jjozic98@gmail.com>
2021-01-13 19:16:15 +0100

    Added tables for group stage

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
+function createRow (id, n, p){
+    const row = document.createElement("tr");
+    const name = document.createElement("td");
+    name.innerText = n;
+    const points = document.createElement("td");
+    points.innerText = p;
+    row.append(name, points);
+    document.querySelector(id).append(row);
+}
+
 function genMatches (nTeams) {
     // url (required), options (optional)
     fetch('https://spreadsheets.google.com/feeds/cells/1gJLOWF7hFOf3YPCAy4MJegMW9da6zd7iF7UqDc225Io/1/public/full?alt=json', {
         method: 'get'
     }).then(function(response) {
         response.json().then(function(data) {
-            // console.log(data);
-            const gA = document.querySelector(".gruppeA");
-            const n1 = document.createElement("span");
-            n1.innerHTML = data.feed.entry[1].gs$cell.inputValue;
-            const n2 = document.createElement("span");
-            n2.innerHTML = data.feed.entry[2].gs$cell.inputValue;
-            const n3 = document.createElement("span");
-            n3.innerHTML = data.feed.entry[3].gs$cell.inputValue;
-            const n4 = document.createElement("span");
-            n4.innerHTML = data.feed.entry[4].gs$cell.inputValue;
-            const n5 = document.createElement("span");
-            n5.innerHTML = data.feed.entry[5].gs$cell.inputValue;
+            //gruppe a
+            for(let i = 1; i<6; i++){
+                let name = data.feed.entry[i].gs$cell.inputValue;
+                let points = data.feed.entry[i+6].gs$cell.inputValue;
+                createRow("#t1", name, points);
+            }
+            //gruppe b
+            for(let i = 13; i<18; i++){
+                let name = data.feed.entry[i].gs$cell.inputValue;
+                let points = data.feed.entry[i+6].gs$cell.inputValue;
+                createRow("#t2", name, points);
+            }
+            //gruppe c
+            for(let i = 25; i<30; i++){
+                let name = data.feed.entry[i].gs$cell.inputValue;
+                let points = data.feed.entry[i+6].gs$cell.inputValue;
+                createRow("#t3", name, points);
+            }
+            //gruppe d
+            for(let i = 37; i<42; i++){
+                let name = data.feed.entry[i].gs$cell.inputValue;
+                let points = data.feed.entry[i+6].gs$cell.inputValue;
+                createRow("#t4", name, points);
+            }
             
-            gA.append(n1,n2,n3,n4,n5);
           });
     }).catch(function(err) {
         // Error :(
